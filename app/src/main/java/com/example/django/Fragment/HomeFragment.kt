72fbaf74ee125566f3b328f5cc58132f4a2fe4f1
Shauni Van de Velde <shauni.vandevelde@gmail.com>
2019-11-28 12:23:51 +0100

    bottom nav works

---
+++ app/src/main/java/com/example/django/Fragment/HomeFragment.kt
@@ -0,0 +1,4 @@
+package com.example.django.Fragment
+
+class HomeFragment {
+}
--- app/src/main/java/com/example/django/Fragment/loginFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.example.django.Fragment
-
-import androidx.fragment.app.Fragment
-
-class loginFragment : Fragment() {
-}
