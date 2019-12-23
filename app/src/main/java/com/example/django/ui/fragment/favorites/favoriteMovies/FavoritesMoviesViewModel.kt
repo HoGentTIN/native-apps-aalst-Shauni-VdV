500852d12ae7a3e9cd140097cc177847349f9849
Shauni Van de Velde <shauni.vandevelde@gmail.com>
2019-12-23 20:30:24 +0100

    Implement Fragments & ViewModels for Favorite movies

--- app/src/main/java/com/example/django/ui/fragment/favorites/favoriteMovies/FavoritesMoviesViewModel.kt
+++ app/src/main/java/com/example/django/ui/fragment/favorites/favoriteMovies/FavoritesMoviesViewModel.kt
@@ -1,4 +1,55 @@
 package com.example.django.ui.fragment.favorites.favoriteMovies
 
-class FavoritesMoviesViewModel {
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.django.App
+import com.example.django.model.Movie
+import com.example.django.model.repository.IMovieRepository
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class FavoritesMoviesViewModel : ViewModel() {
+
+    init {
+        App.appComponent.inject(this)
+        getFavoriteMovies()
+    }
+
+    enum class ApiStatus { LOADING, ERROR, DONE }
+
+
+    @Inject
+    lateinit var movieRepository: IMovieRepository
+
+    private val _favoriteMovies = MutableLiveData<List<Movie>>()
+    val favoriteMovies: LiveData<List<Movie>>
+        get() = _favoriteMovies
+
+    private val _status = MutableLiveData<ApiStatus>()
+    val status: LiveData<ApiStatus>
+        get() = _status
+
+    private val _navigateToSelectedMovie = MutableLiveData<Movie>()
+    val navigateToSelectedMovie: LiveData<Movie>
+        get() = _navigateToSelectedMovie
+
+    private fun getFavoriteMovies() {
+        viewModelScope.launch {
+            var result = movieRepository.getFavoriteMovies()
+            _favoriteMovies.value = result
+        }
+    }
+
+    fun displayMovieDetails(movie: Movie) {
+        _navigateToSelectedMovie.value = movie
+    }
+
+    fun displayMovieDetailsComplete() {
+        _navigateToSelectedMovie.value = null
+    }
+
+
 }
