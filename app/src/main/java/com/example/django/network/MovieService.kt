ff11fd535873c5f88f05f5dbcc6a83d227225bf5
Shauni Van de Velde <shauni.vandevelde@gmail.com>
2019-12-24 13:44:50 +0100

    add api call

--- app/src/main/java/com/example/django/network/MovieService.kt
+++ app/src/main/java/com/example/django/network/MovieService.kt
@@ -2,8 +2,11 @@ package com.example.django.network
 
 import com.example.django.model.Movie
 import com.example.django.network.response.MovieListResponse
+import com.example.django.network.response.SearchResponse
+import io.reactivex.Single
 import kotlinx.coroutines.Deferred
 import retrofit2.http.GET
+import retrofit2.http.Query
 import retrofit2.http.QueryMap
 
 
@@ -21,4 +24,7 @@ interface MovieService {
 
     @GET("3/movie/{movie_id}")
     fun getMovieById(@QueryMap hashMap: HashMap<String, String> = HashMap()): Movie
+
+    @GET("search/multi")
+    fun getSearchResults(@Query("query") query: String, @Query("page") page: Int) : Single<SearchResponse>
 }
