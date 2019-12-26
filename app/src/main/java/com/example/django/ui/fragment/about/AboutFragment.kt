386b59e2f465a9fa30da2e3d8ce121fb192d6a0f
Shauni Van de Velde <shauni.vandevelde@gmail.com>
2019-12-26 13:45:33 +0100

    About page

--- app/src/main/java/com/example/django/ui/fragment/about/AboutFragment.kt
+++ app/src/main/java/com/example/django/ui/fragment/about/AboutFragment.kt
@@ -1,5 +1,8 @@
 package com.example.django.ui.fragment.about
 
+
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,7 +10,28 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.django.R
+import com.example.django.databinding.FragmentAboutBinding
+import kotlinx.android.synthetic.main.fragment_about.*
+
 
 class AboutFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
+        val binding: FragmentAboutBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_about, container, false
+        )
+        activity?.setTitle("About")
+
+        binding.imageView.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW)
+            i.data = Uri.parse("https://www.themoviedb.org/documentation/api")
+            activity!!.startActivity(i)
+        }
+
+        return binding.root
+    }
 }
