b56e5ac000f849b37397c793ad063b1204ca5781
Shauni Van de Velde <shauni.vandevelde@gmail.com>
2019-12-26 10:11:57 +0100

    Create empty fragment 'About'

---
+++ app/src/main/java/com/example/django/ui/fragment/about/AboutFragment.kt
@@ -0,0 +1,13 @@
+package com.example.django.ui.fragment.about
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.django.R
+
+class AboutFragment : Fragment() {
+
+}
